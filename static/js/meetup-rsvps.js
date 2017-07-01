5acd2163b996f692dd5caf755cd3935a3eb54b93
Horst Gutmann <horst@zerokspot.com>
2017-07-01 20:27:02 +0200

    Add RSVP rendering

---
+++ static/js/meetup-rsvps.js
@@ -0,0 +1,24 @@
+(function() {
+    if (typeof MEETUP_ID === 'undefined' || typeof fetch === 'undefined') {
+        return;
+    }
+    fetch('https://api.gograz.org/meetup/' + MEETUP_ID + '/rsvps').then(resp => {
+        return resp.json();
+    }).then(data => {
+        var container = document.getElementById('posRSVPs');
+        if (!container) {
+            return;
+        }
+        data.yes.forEach((member, idx) => {
+            var link = document.createElement('a');
+            link.setAttribute('class', 'member');
+            link.setAttribute('href', 'https://www.meetup.com/Graz-Open-Source-Meetup/members/' + member.id);
+            link.setAttribute('title', member.name);
+            var img = document.createElement('img');
+            img.setAttribute('src', member.thumbLink);
+            img.setAttribute('alt', member.name);
+            link.appendChild(img);
+            container.appendChild(link);
+        });
+    });
+})();
